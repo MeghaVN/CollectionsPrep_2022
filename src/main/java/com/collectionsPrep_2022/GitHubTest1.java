6dad139318b53a68aa47328751bf7b070c95ef4f
megha <megha20vn@gmail.com>
2022-04-27 14:09:18 +0100

    day2 commit -by teamate

--- src/main/java/com/collectionsPrep_2022/GitHubTest1.java
+++ src/main/java/com/collectionsPrep_2022/GitHubTest1.java
@@ -4,6 +4,8 @@ public class GitHubTest1 {
 	public static void main(String[] args) {
 		System.out.println("github...");
 		System.out.println("github..Review done.");
+		System.out.println("github...day2");
+		System.out.println("github..Review done.day2");
 	}
 
 }
